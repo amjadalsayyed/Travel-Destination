4acab3a9167ffcb8d043d8e8b515345f80b7dc50
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-03-11 13:08:43 +0300

    had fun doing this

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./components/home/Home";
+import TourDetails from "./components/tourDetails/TourDetails";
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/city/:id" element={<TourDetails />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
